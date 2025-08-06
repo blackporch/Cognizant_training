cfc0eded37d91886986a72577f5076cac261ea5b
sanchita <bsanchita4321@gmail.com>
2025-08-06 23:08:20 +0530

    feat: added openapi

--- demo/src/main/java/com/example/demo/controller/StudentController.java
+++ demo/src/main/java/com/example/demo/controller/StudentController.java
@@ -11,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api")
 public class StudentController {
     @Autowired
     private StudentService studentService;
